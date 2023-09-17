9966360ea13fa975122d1a997818ed32a83fc12e
vinicius <vinc-analist@hotmail.com>
2023-09-16 21:47:20 -0300

    Initial commit

---
+++ src/main/java/ComparacaoDatas.java
@@ -0,0 +1,32 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+
+public class ComparacaoDatas {
+
+    public static void main(String[] args) {
+        LocalDate dataFutura = LocalDate.of(2023, Month.DECEMBER, 02);
+        LocalDate dataAtual = LocalDate.now();
+
+
+        System.out.println("A data futura é depois da data atual? "
+                + dataFutura.isAfter(dataAtual));
+        System.out.println("A data atual é anterior a data futura? "
+                + dataAtual.isBefore(dataFutura));
+
+        LocalDateTime dataHoraFutura = LocalDateTime.of(2023, Month.SEPTEMBER, 15, 22, 00, 00);
+        LocalDateTime dataHoraAtual = LocalDateTime.now();
+
+        System.out.println("A dataHora futura é depois da dataHora atual? "
+                + dataHoraFutura.isAfter(dataHoraAtual));
+        System.out.println("A dataHora autual é anterior da dataHora futura? "
+                + dataHoraAtual.isBefore(dataHoraFutura));
+
+
+
+
+
+
+
+    }
+}
